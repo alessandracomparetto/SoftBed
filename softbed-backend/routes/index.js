3522b594f106b956d5a7938d016b5189a86597c0
Salvatore Lo Coco <salvatore.lococo.slc@gmail.com>
2020-06-16 13:34:55 +0200

    Connessione DB - Parte 1

--- softbed-backend/routes/index.js
+++ softbed-backend/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SoftBed | La comodità a portata di click' });
+});
+
+/* GET login page. */
+router.get('/accedi', function(req, res, next) {
+  res.render('login', { title: 'Accedi' });
+});
+
+/* GET registration page. */
+router.get('/registrati', function(req, res, next) {
+  res.render('registration', { title: 'Registrazione' });
 });
 
 module.exports = router;
