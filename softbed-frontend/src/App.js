386e3c501743098e30982013a17b65d1e424a0d9
Salvatore Lo Coco <salvatore.lococo.slc@gmail.com>
2020-06-16 10:54:56 +0200

    TEST API

--- softbed-frontend/src/App.js
+++ softbed-frontend/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" }
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/test")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res}))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                </header>
+                <p>{this.state.apiResponse}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
