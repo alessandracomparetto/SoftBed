f2852600c9e2e5f19ad7c923797d8e19f2d3ad2a
Salvatore Lo Coco <salvatore.lococo.slc@gmail.com>
2020-06-17 19:47:05 +0200

    Aggiornamento NavBar e Form Ricerca

--- softbed-frontend/src/App.js
+++ softbed-frontend/src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar";
+import FormRicerca from "./Components/FormRicerca";
 
 function App() {
 
     return (
-        <Navbar />
+        <React.Fragment>
+            <Navbar />
+            <div className="w100 bg-secondary">
+                <FormRicerca />
+            </div>
+        </React.Fragment>
     )
 }
 
--- softbed-frontend/src/Components/Navbar.js
+++ softbed-frontend/src/Components/Navbar.js
@@ -1,62 +1,80 @@
 import React from 'react';
 
-function NavbarToggler() {
+function UtenteNonAutenticato() {
     return (
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <span className="navbar-toggler-icon"/>
-        </button>
+        <React.Fragment>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span className="fas fa-sign-in-alt"/>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul className="navbar-nav ml-auto text-right">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/login/">Accedi</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/registration/">Registrati</a>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
     )
 }
 
-function NuovoUtente() {
+function UtenteAutenticato() {
     return (
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <a className="nav-link" href="/login/">Accedi</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/registration/">Registrati</a>
-                </li>
-            </ul>
-        </div>
-    )
-}
+        <React.Fragment>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span className="fas fa-user"/>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul className="navbar-nav ml-auto text-right">
+                    {/* Dropdown menu | d-sm-flex */}
+                    <li className="nav-item dropdown d-none d-sm-flex">
+                        <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+                            <i className="fas fa-user"/>
+                        </a>
+                        <div className="dropdown-menu dropdown-menu-right">
+                            <a className="dropdown-item" href="#">Modifica profilo</a>
+                            <a className="dropdown-item" href="#">Gestione strutture</a>
+                            <a className="dropdown-item" href="#">Le mie prenotazioni</a>
+                            <div className="dropdown-divider"/>
+                            <a className="dropdown-item" href="#">Esci</a>
+                        </div>
+                    </li>
 
-function UtenteCollegato() {
-    return (
-        <ul className="navbar-nav ml-auto">
-            <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle text-right" href="#" data-toggle="dropdown">
-                    <i className="fas fa-user"/>
-                </a>
-                <div className="dropdown-menu dropdown-menu-right">
-                    <a className="dropdown-item" href="#">Modifica profilo</a>
-                    <a className="dropdown-item" href="#">Gestione struttura</a>
-                    <a className="dropdown-item" href="#">Le mie prenotazioni</a>
-                    <div className="dropdown-divider"/>
-                    <a className="dropdown-item" href="#">Esci</a>
-                </div>
-            </li>
-        </ul>
+                    {/* Collapsed menu | d-sm-none */}
+                    <li className="nav-item d-sm-none">
+                        <a className="nav-link" href="#">Modifica profilo</a>
+                    </li>
+                    <li className="nav-item d-sm-none">
+                        <a className="nav-link" href="#">Gestione strutture</a>
+                    </li>
+                    <li className="nav-item d-sm-none">
+                        <a className="nav-link" href="#">Le mie prenotazioni</a>
+                    </li>
+                    <div className="dropdown-divider d-sm-none"/>
+                    <li className="nav-item d-sm-none">
+                        <a className="nav-link" href="#">Esci</a>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
     )
 }
 
 
 function Navbar() {
     // TODO
-    const accessoEffettuato = false;
+    const accessoEffettuato = true;
 
     return (
         <nav className ="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
             <a className="navbar-brand" href="/">softbed</a>
+
             { accessoEffettuato ? (
-                <UtenteCollegato />
+                <UtenteAutenticato />
             ) : (
-                <React.Fragment>
-                    <NavbarToggler />
-                    <NuovoUtente />
-                </React.Fragment>
+                <UtenteNonAutenticato />
             )}
         </nav>
     )
