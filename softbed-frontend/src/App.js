7615038010f534e2b7ccfe7988393f3799d9d918
Salvatore Lo Coco <salvatore.lococo.slc@gmail.com>
2020-06-18 02:54:24 +0200

    Logica di frontend per il form di ricerca.

--- softbed-frontend/src/App.js
+++ softbed-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar";
 import FormRicerca from "./Components/FormRicerca";
@@ -9,7 +8,7 @@ function App() {
     return (
         <React.Fragment>
             <Navbar />
-            <div className="w100 bg-secondary">
+            <div className="w100">
                 <FormRicerca />
             </div>
         </React.Fragment>
--- softbed-frontend/src/Components/FormRicerca.js
+++ softbed-frontend/src/Components/FormRicerca.js
@@ -1,40 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
 function FormRicerca() {
-    /* TODO:
-     * dovremmo ottenere una lista delle località (regioni, province e comuni) con almeno una struttura per suggerire
+    /* TODO: BACKEND
+     * Gestione props nel caso di richiesta GET
+     */
+
+
+    /* TODO: BACKEND
+     * Lista delle località (regioni, province e comuni) con almeno una struttura per suggerire
      * le opzioni disponibili mentre si digita la destinazione
      */
 
-    /* TODO: bisogna gestire le date */
-    // Data di arrivo minima: almeno x giorni dopo la data attuale
-    // Data di partenza minima: almeno y giorni dopo la data di arrivo selezionata
-    const domani = "2020-06-18"
-    const dopodomani = "2020-06-19"
-    const maxData = "2021-12-31"
+    const GIORNO = 86400000;
+    const dataAttuale = new Date();
+
+    // Il gestore deve avere almeno 48 ore per confermare o rifiutare la richiesta per cui la prima data disponibile
+    // sarà 2 giorni dopo la data in cui viene effettuata la ricerca (3 se dopo le 12:00)
+    const primaDataArrivo = new Date(dataAttuale.getTime() + 2.5 * GIORNO);
+    const minDataA = convertiData(primaDataArrivo);
+
+    // Il soggiorno deve durare almeno 1 giorno
+    const primaDataPartenza = new Date(primaDataArrivo.getTime() + GIORNO);
+
+    // Si assume che la data massima inseribile sia il 31 Dicembre dell'anno successivo (un po' eccessiva)
+    const maxData = (dataAttuale.getFullYear() + 1) + "-12-31"
+
+    const [minDataP, setMinDataP] = useState(convertiData(primaDataPartenza));
+
+    // Converte la data da oggetto Date a stringa in formato "AAAA-MM-GG"
+    function convertiData (data) {
+        const giorno = data.getDate().toString().padStart(2, "0");
+        // +1 poiché getMonth() restituisce valori da 0 (Gennaio) a 11 (Dicembre).
+        const mese = (data.getMonth() + 1).toString().padStart(2, "0");
+        const anno = data.getFullYear();
+
+        return anno + "-" + mese + "-" + giorno;
+    }
+
+    // Aggiorna il valore minimo per la data di partenza in base alla data di arrivo inserita
+    const aggiornaMinDataPartenza = (event) => {
+        const dataInserita = new Date(event.target.value);
+        const nuovaData = new Date(dataInserita.getTime() + GIORNO);
+        console.log(nuovaData);
+        const nuovaDataConvertita = convertiData(nuovaData);
+        console.log(nuovaDataConvertita);
+        setMinDataP(nuovaDataConvertita);
+    }
 
     return (
         <form className="form pt-3 pl-5 pr-5 d-flex justify-content-center">
             <div className="form-row maxw-lg">
-                <div className="col-12 col-lg-3 mb-3">
+                <div className="col-12 col-lg-4 mb-3">
                     <label htmlFor="destinazione">Destinazione</label>
-                    {/* TODO: opterei per una select con input */}
-                    <input name="destinazione" id="destinazione" type="text" className="form-control" required/>
+                    <input name="destinazione" id="destinazione" type="text" className="form-control" placeholder="Inserisci la tua destinazione..." required/>
                 </div>
 
                 <div className="col-12 col-sm-6 col-md-3 mb-3">
                     <label htmlFor="arrivo">Arrivo</label>
-                    <input name="arrivo" id="arrivo" type="date" className="form-control" min={domani} max={maxData} value={domani} required/>
+                    <input name="arrivo" id="arrivo" type="date" className="form-control" min={minDataA} max={maxData} defaultValue={minDataA} onChange={aggiornaMinDataPartenza} required/>
                 </div>
 
                 <div className="col-12 col-sm-6 col-md-3 mb-3">
                     <label htmlFor="partenza">Partenza</label>
-                    <input name="partenza" id="partenza" type="date" className="form-control" min={dopodomani} max={maxData} value={dopodomani} required/>
+                    <input name="partenza" id="partenza" type="date" className="form-control" min={minDataP} max={maxData} defaultValue={minDataP} required/>
                 </div>
 
-                <div className="col-md-3 col-lg-2 mb-3">
+                <div className="col-12 col-md-3 col-lg-1 mb-3">
                     <label htmlFor="Ospiti">Ospiti</label>
-                    <input name="ospiti" id="ospiti" type="number" className="form-control" min={1} max={99} value={2} required/>
+                    <input name="ospiti" id="ospiti" type="number" className="form-control" min={1} max={99} defaultValue={2} required/>
                 </div>
 
                 <div className="mt-auto col-md-3 col-lg-1 mb-3">
