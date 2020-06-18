466df3769d57ead9862b9d9c4affabc40f938218
alessandracomparetto <alessandra.comparetto@gmail.com>
2020-06-18 14:52:53 +0200

    Refactor di ButtonForm e aggiornamento di App.css

---
+++ softbed-frontend/src/Components/ButtonForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ButtonForm(){
+    return(
+    <div className="container-fluid p-0 mt-2 mb-5 d-flex justify-content-between">
+        <button name="indietro" id="indietro" className="btn btn-secondary mt-3 float-left btn-lg w-50px ">Indietro</button>
+        <button name="ok" id="ok" type="submit" className="btn btn-primary mt-3  flaot-right btn-lg w-50px">Continua</button>
+    </div>
+    )
+}
+export default ButtonForm
