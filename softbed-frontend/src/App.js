19aa0832eeeb8d82d8f02bc0b5dc4cf597537fa4
sofiacatalano <sofia.catalano02@community.unipa.it>
2020-06-18 23:06:16 +0200

    Dati pagamento

--- softbed-frontend/src/App.js
+++ softbed-frontend/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Navbar from "./Components/Navbar";
 import FormDatiAggiuntivi from "./Components/FormDatiAggiuntivi";
+import FormDatiOspite from "./Components/FormDatiOspite";
+import SchermataDatoPagamento from "./Components/SchermataDatoPagamento";
+import Login from "./Components/Login";
+import Registration from "./Components/Registration";
 
 function App() {
 
     return (
         <React.Fragment>
             <Navbar />
-            <FormDatiAggiuntivi />
+            <FormDatiOspite/>
         </React.Fragment>
     )
 }
