fc88304d440afb463bc954fd5308932c1f5356fc
alessandracomparetto <alessandra.comparetto@gmail.com>
2020-06-18 22:14:59 +0200

    Form Camere funzionante con logica per l'inserimento delle camere nella lista

--- softbed-frontend/src/Components/FormCamere.js
+++ softbed-frontend/src/Components/FormCamere.js
@@ -1,25 +1,71 @@
-import React from "react";
+import React from 'react';
 import ButtonForm from "./ButtonForm";
 
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-// Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
-
+/*TODO: controllare bene i valori di default e required del form*/
 function FormCamere(){
+    const camera = "Camera ";
+    let contatore = 0;
+
+    function scriviCamera() {
+        let lista = document.getElementById("listaCamere");
+        let tipologia = document.getElementById("tipologia");
+        let nlettiMatrimoniali = document.getElementById("nLettiMatrimoniali");
+        let nlettiSingoli = document.getElementById("nLettiSingoli");
+        let nCamere = document.getElementById("nCamere");
+        let prezzo = document.getElementById("prezzo");
+        let flag = false;
+
+        if (tipologia.value === "") {
+            tipologia.classList.add("border-warning");
+            document.getElementById("indicazioneTipologia").classList.remove("collapse");
+            flag=true;
+
+        }
+        if (nlettiMatrimoniali.value + nlettiSingoli.value < 1) {
+            nlettiMatrimoniali.classList.add("border-warning");
+            nlettiSingoli.classList.add("border-warning");
+            let messaggio = document.getElementsByClassName("indicazioneLetti")
+            for (let i = 0; i < messaggio.length; i++) {
+                messaggio[i].classList.remove("collapse");
+            }
+            flag=true;
+        }
+
+        if (prezzo.checkValidity() === false) {
+            prezzo.classList.add("border-warning");
+            document.getElementById("indicazionePrezzo").classList.remove("collapse");
+            flag=true;
+        }
+
+        if(!flag) {
+            if (contatore===0) {
+                //rimuovo lo spazio vuoto dentro lo scrollable
+                lista.removeChild(lista.childNodes[0]);
+            }
+            for(let i = 0; i<nCamere.value; i++){
+                contatore++
+                let p = document.createElement("P");
+                let info = ":\t "+ tipologia.value +", Letti Matrimoniali: " +nlettiMatrimoniali.value+", Letti Singoli: "+nlettiSingoli.value+", Prezzo: "+prezzo.value;
+                let stringa = document.createTextNode(camera + contatore+ info);
+                p.appendChild(stringa);
+                lista.appendChild(p);
+            }
+            //azzero tutto dopo l'aggiunta
+            tipologia.value = "";
+            nlettiMatrimoniali.value = 0;
+            nlettiSingoli.value = 0;
+            nCamere.value = 1;
+            prezzo.value = null;
+            let listaCollapse = document.getElementsByClassName("messaggio");
+            let listaWarning = document.getElementsByClassName("border-warning");
+            for(let i=0; i<listaCollapse.length;i++ ){
+                listaCollapse[0].classList.add("collapse")
+            }
+            while(listaWarning.length>0){
+                listaWarning[0].classList.remove("border-warning")
+            }
+        }
+    }
     return(
         <form className="container col-12 col-md-8 needs-validation" noValidate>
             <h6 className="mt-3 border-bottom border-primary">Camere presenti</h6>
@@ -33,37 +79,35 @@ function FormCamere(){
             </div>
 
             <div className="form-group bootstrap-select-wrapper">
-                <label htmlFor="tipologiaCamera">Tipologia di camera</label>
-                <select className="form-control selectpicker" required>
-                    <option value="" disabled selected>Scegli una opzione</option>
+                <label htmlFor="tipologiaCamera" >Tipologia di camera</label>
+                <select className="form-control selectpicker" id="tipologia" defaultValue="" required>
+                    <option value="">Scegli una opzione</option>
                     <option value="Singola">Singola</option>
                     <option value="Doppia">Doppia</option>
                     <option value="Tripla">Tripla</option>
                     <option value="Quadrupla">Quadrupla</option>
                 </select>
+                <small  id="indicazioneTipologia" className="form-text text-muted collapse messaggio">Per registrare la camera devi aver scelto la tipologia</small>
             </div>
 
 
             <div className="form-group">
                 <label htmlFor="nLettiMatrimoniali">Numero letti matrimoniali</label>
                 <input name="nLettiMatrimoniali" id="nLettiMatrimoniali" type="number" className="form-control" min="0" max="10" size="2" maxLength="2" required/>
-                <div className="invalid-feedback">
-                    1 - 10
-                </div>
+                <div className="invalid-feedback">1 - 10</div>
+                <small  className="form-text text-muted collapse indicazioneLetti messaggio">Per registrare la camera devi aver inserito almeno un letto</small>
+
             </div>
             <div className="form-group">
                 <label htmlFor="nLettiSingoli">Numero letti singoli </label>
-                <input name="nLettiSingoli" id="nLettiSingoli" type="number" className="form-control" min="0" max="10" size="2" maxLength="2" required />
-                <div className="invalid-feedback">
-                    1 - 10
-                </div>
+                <input name="nLettiSingoli" id="nLettiSingoli" type="number" className="form-control" min="0" max="10" size="2" maxLength="2" required/>
+                <div className="invalid-feedback">1 - 10</div>
+                <small className="form-text text-muted collapse indicazioneLetti messaggio">Per registrare la camera devi aver inserito almeno un letto</small>
             </div>
             <div className="form-group">
                 <label htmlFor="nCamere">Numero di camere per questa tipologia </label>
-                <input name="nCamere" id="nCamere" type="number" className="form-control" min="1" max="10" size="2" maxLength="2" required />
-                <div className="invalid-feedback">
-                    1 - 10
-                </div>
+                <input name="nCamere" id="nCamere" type="number" className="form-control" min="1" max="10" size="2" maxLength="2"  defaultValue="1"/>
+                <div className="invalid-feedback">1 - 10</div>
             </div>
 
             <div className="form-group">
@@ -74,13 +118,12 @@ function FormCamere(){
                     </div>
                     <input name="prezzo" id="prezzo" type="number" className="form-control currency" min="0" step="0.01" max="10000" required/>
                 </div>
-                <div className="invalid-feedback">
-                    Inserire il prezzo base a notte
-                </div>
+                <div className="invalid-feedback">Inserire il prezzo base a notte</div>
+                <small id="indicazionePrezzo" className="form-text text-muted collapse messaggio">Inserire il prezzo base a notte</small>
             </div>
 
             <div className="d-flex justify-content-end">
-                <button className="btn btn-outline-primary">
+                <button className="btn btn-outline-primary" type="button" onClick={scriviCamera}>
                     Aggiungi camera
                 </button>
             </div>
