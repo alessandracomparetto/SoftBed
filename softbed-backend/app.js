f6b8a33ff48c885397b8a936e140c79426832753
alessandracomparetto <alessandra.comparetto@gmail.com>
2020-06-19 22:25:27 +0200

    Aggiunta di alcuni commenti in FormCamere, eliminazione di App.test.js, eliminazione di logo.svg, cambiamento del titolo della pagina in index.html, nuove dipendenze a backend, nuova rotta upload.js (ancora da testare)

--- softbed-backend/app.js
+++ softbed-backend/app.js
@@ -5,11 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var softbedAPI = require('./routes/softbedAPI');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const upload = require('./routes/upload');
+const fileUpload = require('express-fileupload');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/test", softbedAPI);
+app.use(fileUpload());
+//app.use('/upload', upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,6 +42,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
 
+});
 module.exports = app;
