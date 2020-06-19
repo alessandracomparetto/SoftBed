8b6e5357c5efb6de9bf368cccfcd9a84cf1faeb7
mary_ <mary_pal@live.it>
2020-06-19 20:31:17 +0200

    Modifiche FormCaratteristicheB.js e FormCaratteristicheC.js. Aggiunta della progressBar

--- softbed-frontend/src/Components/FormCaratteristicheB.js
+++ softbed-frontend/src/Components/FormCaratteristicheB.js
@@ -1,98 +1,125 @@
 import React from "react";
+import ButtonForm from "./ButtonForm";
+
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        }, false);
+    });
+    }, false);
+})();
 
 function FormCaratteristicheB(){
-return(
-<div className="container">
-  <h2>Caratteristiche</h2>
-  <form className="w50 justify-content-center mt-3">
-  <h6 className="mt-3 border-bottom border-primary">Servizi disponibili</h6>
-      <div className="form-group text-center">
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label">
-                  <input type="checkbox" className="form-check-input" value="something" name="connessione"
-                         />Connessione Wi-fi
-              </label>
+    function verificaLunghezza(event){
+        if(event.target.value.length>=200){
+            document.getElementById("feedback").classList.remove("collapse");
+            event.target.classList.add("border-warning");
+        }
+        else{
+            document.getElementById("feedback").classList.add("collapse");
+            event.target.classList.remove("border-warning");
+        }
+    }
+    return(
+        <div className="container">
+        <div className="progress">
+            <div className="progress-bar" style={{width: 60 + '%'}}>60%</div>
+        </div>
+        <h4>Caratteristiche B&B</h4>
+        <form className="w50 justify-content-center mt-3 needs-validation" noValidate>
+          <h6 className="mt-3 border-bottom border-primary">Servizi disponibili</h6>
+          <div className="form-row-group text-center">
+              <div className="form-check-inline col-12  col-sm-5  col-lg-3">
+                  <label className="form-check-label">
+                      <input type="checkbox" className="form-check-input " value="true" name="connessione"
+                      />Connessione Wi-fi
+                  </label>
+              </div>
+              <div className="form-check-inline col-12  col-sm-5 col-lg-3">
+                  <label className="form-check-label" htmlFor="riscaldamento">
+                      <input type="checkbox" className="form-check-input" id="riscaldamento" name="riscaldamento"
+                             value="true"/>Riscaldamento
+                  </label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3">
+                  <label className="form-check-label" htmlFor="cucina">
+                      <input type="checkbox" className="form-check-input" id="cucina" name="cucina" value="true"/>Cucina
+                      per celiaci
+                  </label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3">
+                  <label className="form-check-label" htmlFor="disabili">
+                      <input type="checkbox" className="form-check-input" id="disabili" name="disabili" value="true"/>Strutture
+                      per disabili
+                  </label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3">
+                  <label className="form-check-label" htmlFor="aria">
+                      <input type="checkbox" className="form-check-input" id="aria" name="aria" value="true"/>Aria
+                      condizionata
+                  </label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3">
+                  <label className="form-check-label" htmlFor="tv">
+                      <input type="checkbox" className="form-check-input" id="tv" name="tv" value="true"/>TV
+                  </label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3">
+                  <label className="form-check-label" htmlFor="navettaAereoportuale">
+                      <input type="checkbox" className="form-check-input" id="navettaAereoportuale"
+                             name="navettaAereoportuale" value="true"/>Navetta aereoportuale
+                  </label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3">
+                  <label className="form-check-label" htmlFor="parcheggio">
+                      <input type="checkbox" className="form-check-input" id="parcheggio" name="parcheggio"
+                             value="true"/>Parcheggio
+                  </label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3 ">
+                  <label className="form-check-label " htmlFor="servizioInCamera">
+                      <input type="checkbox" className="form-check-input" id="servizioInCamera" name="servizioInCamera"
+                             value="true"/>Servizio in camera
+                  </label>
+              </div>
           </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="riscaldamento">
-                  <input type="checkbox" className="form-check-input" id="riscaldamento" name="riscaldamento"
-                         value="something"/>Riscaldamento
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="cucina">
-                  <input type="checkbox" className="form-check-input" id="cucina" name="cucina" value="something"/>Cucina
-                  per celiaci
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="disabili">
-                  <input type="checkbox" className="form-check-input" id="disabili" name="disabili" value="something"/>Strutture
-                  per disabili
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="aria">
-                  <input type="checkbox" className="form-check-input" id="aria" name="aria" value="something"/>Aria
-                  condizionata
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="tv">
-                  <input type="checkbox" className="form-check-input" id="tv" name="tv" value="something"/>TV
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="navettaAereoportuale">
-                  <input type="checkbox" className="form-check-input" id="navettaAereoportuale"
-                         name="navettaAereoportuale" value="something"/>Navetta aereoportuale
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="parcheggio">
-                  <input type="checkbox" className="form-check-input" id="parcheggio" name="parcheggio"
-                         value="something"/>Parcheggio
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="servizioInCamera">
-                  <input type="checkbox" className="form-check-input" id="servizioInCamera" name="servizioInCamera"
-                         value="something"/>Servizio in camera
-              </label>
-          </div>
-      </div>
 
-    <h6 className="mt-3 border-bottom border-primary">Sugli ospiti</h6>
-      <div className="form-group text-center">
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="bambini">
-                  <input type="checkbox" className="form-check-input" id="bambini" name="bambini" value="something"/>Idoneità
-                  ad ospitare bambini
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="animali">
-                  <input type="checkbox" className="form-check-input" id="animali" name="animali" value="something"/>Animali
-                  ammessi
-              </label>
-          </div>
-          <div className="form-check-inline col-12 col-md-4 col-lg-3">
-              <label className="form-check-label" htmlFor="permessoFumo">
-                  <input type="checkbox" className="form-check-input" id="permessoFumo" name="permessoFumo"
-                         value="something"/>Permesso di fumare
-              </label>
-          </div>
-      </div>
 
-      <h6 className="mt-3 border-bottom border-primary">Descrizione</h6>
-      <div className="md-form amber-textarea active-amber-textarea">
-      <textarea id="descrizione" className="md-textarea form-control" rows="5"  placeholder="Write something here..."></textarea>
-    </div>
-  </form>
-    <div className="d-flex col-12 col-sm-10 mx-auto justify-content-between m-5">
-        <button type="button" className="btn btn-outline-danger btn-lg">Indietro</button>
-        <button type="button" className="btn btn-primary col-3 btn-block">Continua</button>
-    </div>
-</div>
+        <h6 className="mt-3 border-bottom border-primary">Sugli ospiti</h6>
+          <div className="form-group text-center">
+              <div className="form-check-inline  col-12 col-sm-5 col-lg-3 ">
+                  <input type="checkbox" className="form-check-input" id="animali" name="animali" value="true"/>
+                  <label className="form-check-label" htmlFor="animali">Animali ammessi</label>
+              </div>
+              <div className="form-check-inline  col-12 col-sm-5 col-lg-3 ">
+                  <input type="checkbox" className="form-check-input" id="permessoFumo" name="permessoFumo" value="true"/>
+                  <label className="form-check-label" htmlFor="permessoFumo">Permesso di fumare</label>
+              </div>
+              <div className="form-check-inline col-12 col-sm-5 col-lg-3 ">
+                  <input type="checkbox" className="form-check-input" id="bambini" name="bambini" value="true"/>
+                  <label className="form-check-label" htmlFor="bambini">Idoneità ad ospitare bambini </label>
+              </div>
+          </div>
+
+          <h6 className="mt-3 border-bottom border-primary">Descrizione</h6>
+            <div className="md-form amber-textarea active-amber-textarea">
+                <textarea id="descrizione" name="descrizione" className="md-textarea form-control" rows="5"  maxLength="200" placeholder="Write something here..." onChange={verificaLunghezza}></textarea>
+                <p id="feedback" className="text-danger form-text text-muted collapse ">Massimo 200 caratteri</p>
+            </div>
+        <ButtonForm/>
+        </form>
+
+        </div>
 )}
 export default FormCaratteristicheB
--- softbed-frontend/src/Components/FormCaratteristicheC.js
+++ softbed-frontend/src/Components/FormCaratteristicheC.js
@@ -1,84 +1,104 @@
 import React from "react";
+import ButtonForm from "./ButtonForm";
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Get the forms we want to add validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
 
 function FormCaratteristicheC(){
+    function verificaLunghezza(event){
+        if(event.target.value.length>=200){
+            document.getElementById("feedback").classList.remove("collapse");
+            event.target.classList.add("border-warning");
+        }
+        else{
+            document.getElementById("feedback").classList.add("collapse");
+            event.target.classList.remove("border-warning");
+        }
+    }
     return(
+
         <div className="container">
-            <h2>Caratteristiche</h2>
-            <form className="w50 justify-content-center mt-3">
+            <div className="progress">
+                <div className="progress-bar" style={{width: 60 + '%'}}>60%</div>
+            </div>
+            <h4>Caratteristiche Casa vacanze</h4>
+            <form className="w50  mt-3 needs-validation" noValidate>
                 <h6 className="mt-3 border-bottom border-primary">Servizi disponibili</h6>
-                <div className="form-group text-center">
-                    <div className="form-check-inline col-12 col-md-4 col-lg-3">
-                        <label className="form-check-label">
-                            <input type="checkbox" className="form-check-input" value="something" name="connessione"
-                            />Connessione Wi-fi
+                <div className="form-row-group text-center">
+                    <div className="form-check-inline col-12 col-md-5">
+                        <input type="checkbox" className="form-check-input" value="true" name="connessione"/>
+                        <label className="form-check-label">Connessione Wi-fi</label>
+                    </div>
+                    <div className="form-check-inline col-12 col-md-5">
+                        <input type="checkbox" className="form-check-input" id="riscaldamento" name="riscaldamento"
+                               value="true"/>
+                        <label className="form-check-label" htmlFor="riscaldamento">Riscaldamento</label>
+                    </div>
+                    <div className="form-check-inline col-12 col-md-5 ">
+                        <input type="checkbox" className="form-check-input" id="disabili" name="disabili" value="true"/>
+                        <label className="form-check-label" htmlFor="disabili">Strutture per disabili
                         </label>
                     </div>
-                    <div className="form-check-inline col-12 col-md-4 col-lg-3">
-                        <label className="form-check-label" htmlFor="riscaldamento">
-                            <input type="checkbox" className="form-check-input" id="riscaldamento" name="riscaldamento"
-                                   value="something"/>Riscaldamento
-                        </label>
+                    <div className="form-check-inline col-12 col-md-5 ">
+                        <input type="checkbox" className="form-check-input" id="aria" name="aria" value="true"/>
+                        <label className="form-check-label" htmlFor="aria"> Aria condizionata</label>
                     </div>
-                    <div className="form-check-inline col-12 col-md-4 col-lg-3">
-                        <label className="form-check-label" htmlFor="disabili">
-                            <input type="checkbox" className="form-check-input" id="disabili" name="disabili" value="something"/>Strutture
-                            per disabili
-                        </label>
+                    <div className="form-check-inline col-12 col-md-5 ">
+                        <input type="checkbox" className="form-check-input" id="tv" name="tv" value="true"/>
+                        <label className="form-check-label" htmlFor="tv">TV</label>
                     </div>
-                    <div className="form-check-inline col-12 col-md-4 col-lg-3">
-                        <label className="form-check-label" htmlFor="aria">
-                            <input type="checkbox" className="form-check-input" id="aria" name="aria" value="something"/>Aria
-                            condizionata
-                        </label>
-                    </div>
-                    <div className="form-check-inline col-12 col-md-4 col-lg-3">
-                        <label className="form-check-label" htmlFor="tv">
-                            <input type="checkbox" className="form-check-input" id="tv" name="tv" value="something"/>TV
-                        </label>
-                    </div>
-                    <div className="form-check-inline col-12 col-md-4 col-lg-3">
-                        <label className="form-check-label" htmlFor="parcheggio">
-                            <input type="checkbox" className="form-check-input" id="parcheggio" name="parcheggio"
-                                   value="something"/>Parcheggio
-                        </label>
+                    <div className="form-check-inline col-12 col-md-5 ">
+                        <input type="checkbox" className="form-check-input" id="parcheggio" name="parcheggio"
+                               value="true"/>
+                        <label className="form-check-label" htmlFor="parcheggio">Parcheggio</label>
                     </div>
                 </div>
 
                 <h6 className="mt-3 border-bottom border-primary">Sugli ospiti</h6>
-                <div className="form-group text-center">
-                    <div className="form-check-inline col-12 col-lg-4">
+                <div className="form-row-group text-center">
+                    <div className="form-check-inline col-12 col-md-5">
+                        <input type="checkbox" className="form-check-input" id="feste" name="feste" value="true"/>
                         <label className="form-check-label" htmlFor="feste">
-                            <input type="checkbox" className="form-check-input" id="feste" name="feste" value="something"/>Permesse per feste/eventi
-                        </label>
+                            Permesse per feste/eventi</label>
                     </div>
-                    <div className="form-check-inline col-12 col-lg-4">
-                        <label className="form-check-label" htmlFor="animali">
-                            <input type="checkbox" className="form-check-input" id="animali" name="animali" value="something"/>Animali ammessi
-                        </label>
+                    <div className="form-check-inline col-12 col-md-5">
+                        <input type="checkbox" className="form-check-input" id="animali" name="animali" value="true"/>
+                        <label className="form-check-label" htmlFor="animali">Animali ammessi</label>
                     </div>
-                    <div className="form-check-inline col-12 col-lg-4">
-                        <label className="form-check-label" htmlFor="permessoFumo">
-                            <input type="checkbox" className="form-check-input" id="permessoFumo" name="permessoFumo"
-                                   value="something"/>Permesso di fumare
-                        </label>
+                    <div className="form-check-inline col-12 col-md-5">
+                        <input type="checkbox" className="form-check-input" id="permessoFumo" name="permessoFumo"
+                               value="true"/>
+                        <label className="form-check-label" htmlFor="permessoFumo">Permesso di fumare</label>
                     </div>
-                    <div className="form-check-inline col-12 col-lg-4 ">
-                        <label className="form-check-label" htmlFor="bambini">
-                            <input type="checkbox" className="form-check-input" id="bambini" name="bambini" value="something"/>Idoneità
-                            ad ospitare bambini
-                        </label>
+                    <div className="form-check-inline col-12 col-md-5">
+                        <input type="checkbox" className="form-check-input" id="bambini" name="bambini" value="true"/>Idoneità
+                        ad ospitare bambini
+                        <label className="form-check-label" htmlFor="bambini"></label>
                     </div>
                 </div>
 
                 <h6 className="mt-3 border-bottom border-primary">Descrizione</h6>
                 <div className="md-form amber-textarea active-amber-textarea">
-                    <textarea id="descrizione" className="md-textarea form-control" rows="5"  placeholder="Write something here..."></textarea>
+                    <textarea id="descrizione" name="descrizione" className="md-textarea form-control" rows="5"  maxLength="200" placeholder="Write something here..." onChange={verificaLunghezza}></textarea>
+                    <p id="feedback" className="text-danger form-text text-muted collapse ">Massimo 200 caratteri</p>
                 </div>
+
+                <ButtonForm/>
             </form>
-            <div className="d-flex col-12 col-sm-10 mx-auto justify-content-between m-5">
-                <button type="button" className="btn btn-outline-danger btn-lg">Indietro</button>
-                <button type="button" className="btn btn-primary col-3 btn-block">Continua</button>
-            </div>
         </div>
     )}
 export default FormCaratteristicheC
