d4b8750ec3fa0de718ad878b7668b823a3d1f583
mary_ <mary_pal@live.it>
2020-06-20 22:51:10 +0200

    Aggiunta semplice Fotter

--- softbed-frontend/src/Components/Footer.js
+++ softbed-frontend/src/Components/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 function Footer(){
     return(
-
         <footer class="page-footer font-small unique-color-dark pt-4">
             <div class="footer-copyright text-center py-3">© 2020 Copyright:
                 SoftEngineers
