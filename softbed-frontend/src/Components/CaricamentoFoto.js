1ff642250fbd38377201551b6a99bb5f9a7be9f2
alessandracomparetto <alessandra.comparetto@gmail.com>
2020-06-21 01:26:29 +0200

    Caricamento foto logica ultimata, modifiche invisibili

--- softbed-frontend/src/Components/CaricamentoFoto.js
+++ softbed-frontend/src/Components/CaricamentoFoto.js
@@ -7,39 +7,37 @@ function CaricamentoFoto(){
     const [filename, setFilename] = useState([]);
     let fileList = {};
 
-    useEffect(() => {
-      console.log(filename);
-    }, [filename]);
-    //quando viene modificato filename lo stampa
-
 
     const onChange = (event) =>{
         fileList = event.target.files;
         setFile(fileList);
-        setNumero(fileList.length);
+        setNumero(numero + fileList.length);
     }
 
     const onSubmit = async e => {
         e.preventDefault();
         const data = new FormData()
         for(var i = 0; i<file.length; i++) {
-            data.append('file', file[i])
+            data.append('file', file[i]);
         }
 
         axios.post("/upload", data)
             .then(res => { // then print response status
-                var nomi = [];
+                var nomi = filename;
+                var dimensione = filename.length;
                 for(let i = 0; i<file.length; i++){
-                    nomi.push({index: i, name: file[i].name});
-                    console.log(file[i].name);
+                    nomi.push({index: dimensione+i, name: file[i].name});
                 }
+                setFile([]);
                 setFilename(nomi);
+                console.log(nomi);
                 console.log(res.statusText)
             })
+
     };
 
     return (
-        <Fragment>
+        <div className="col-10 mx-auto">
             <form encType="multipart/form-data" onSubmit={onSubmit}>
                 <div className="custom-file mb-4">
                     <input name="file" type="file" className="custom-file-input" id="customFile" multiple onChange={onChange}/>
@@ -47,24 +45,22 @@ function CaricamentoFoto(){
                 </div>
                 <input type="submit" value="caricamento" className="btn btn-primary btn-block mt-4" accept="image/png"/>
             </form>
-            { filename&&(
+            { filename &&(
                 <div className="row mt-5">
-                    <div className="col-md-6 m-auto">
-                        {filename.map((f, indice) => {
-                            console.log(f);
-                            return (
-                                <React.Fragment key={indice}>
-                                    <h3 className="text-center"> {f.name}</h3>
-                                    <img style={{ width:'100%'}} src={`/uploads/`+f.name} alt=""/>
-                                </React.Fragment>
-                            )
-                        })}
-                    </div>
+                    {filename.map((f, indice) => {
+                        console.log(f);
+                        return (
+                            <div key={indice} className=" col-12 col-sm-6 col-md-4 col-lg-3">
+                                <p className="text-center"> {f.name}</p>
+                                <img style={{ width:'100%'}} src={`/uploads/`+f.name} alt=""/>
+                            </div>
+                        )
+                    })}
                 </div>)
             }
 
 
-        </Fragment>
+        </div>
     );
 }
 export default CaricamentoFoto;
