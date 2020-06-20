dedc00b4ba8da06df1b5a79d343b00c8c87bbc0b
alessandracomparetto <alessandra.comparetto@gmail.com>
2020-06-20 15:29:08 +0200

    Finalmente carica le immagini, da aggiustare per errori di GET

--- softbed-backend/routes/upload.js
+++ softbed-backend/routes/upload.js
@@ -1,96 +1,31 @@
-/*const express = require('express');
-const createError = require('http-errors');
-const router = express.Router();
+const express = require('express');
 const fileUpload = require('express-fileupload');
+
+const router = express.Router();
 
 router.use(fileUpload());
 
-/!* La rotta /upload è vietata per le richieste get*!/
+/* La rotta /users è vietata */
 router.get('/', function(req, res, next) {
     next(createError(403));
 });
 
-//Mando qui le richieste di upload
-router.post('/', function (req, res) {
-    console.log(req.files)
-    var file_;
-    if(!req.files){
-        console.log("backend dopo if")
-        res.send("file not found");
-        return
+// Upload Endpoint
+router.post('/', (req, res) => {
+    if (req.files === null) {
+        console.log("no get1");
+        return res.status(400).json({ msg: 'No file uploaded' });
     }
-    file_ = req.files.file;
-    res.send("file up");
-});*/
+    const file = req.files.file;
 
-/*router.post('/upload', (req, res) => {
-  if(req.files === null){
-    return res.status(400).json({msg: 'No file uploaded'})
-    //in questo caso creo una risposta BAD REQUEST e mando un messaggio
-  }
-  const file = req.files.file;
-  const uploadPath = path.resolve(__dirname, '../../softend-frontend/public/uploads')
-  file.mv(uploadPath+`/${file.name}`, err =>{
-    if(err){
-      //stampa se si verifica qualche errore
-      console.log(err);
-      return res.status(500).send(err);
-    }
-    res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
-  });
-});*/
 
-/*router.post('/upload', (req, res) => {
-  console.log(req.files);
-  const file = req.files[0];
-   res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
-  /!*if(!req.files){
-    console.log("Uffity");
-    return res.send("IO SONO DUCI");
-    /!*return res.status(400).json({msg: 'No file uploaded'})*!/
-    //in questo caso creo una risposta BAD REQUEST e mando un messaggio
-  }
-  const file = req.files.file;
-
-  //in frontend formData.append('file')
-  //const uploadPath = path.resolve(__dirname, '../../softend-frontend/public/uploads')
-  console.log("banane");
-  file.mv(`/${file.name}`, err =>{
-    if(err){
-      //stampa se si verifica qualche errore
-      console.log(err);
-      return res.status(500).send(err);
-    }
-    res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
-  });*!/
-});*/
-
-/*
-const http = require('http');
-const express = require('express');
-const    Busboy = require('busboy');
-const    path = require('path');
-const fs = require('fs');
-
-var router = express.Router();
-
-router.post('/', function (req, res) {
-    res.send("ciao");
-    var busboy = new Busboy({ headers: req.headers });
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-        console.log("ciao 2");
-        var saveTo = path.join(__dirname, 'uploads/' + filename);
-        file.pipe(fs.createWriteStream(saveTo));
+    file.mv(`${__dirname}/uploads/${file.name}`, err => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
     });
-
-    busboy.on('finish', function() {
-        res.writeHead(200, { 'Connection': 'close' });
-        res.end("That's all folks!");
-    });
-
-    return req.pipe(busboy);
 });
-
 module.exports = router;
-*/
+
 
--- softbed-frontend/src/Components/CaricamentoFoto.js
+++ softbed-frontend/src/Components/CaricamentoFoto.js
@@ -13,41 +13,36 @@ function CaricamentoFoto(){
         setFilename(event.target.files[0].name);
     }
 
-    const onSubmit = async event =>{
-        event.preventDefault();
-        //per mandare il file:
-       let formData = new FormData();
+    const onSubmit = async e => {
+        e.preventDefault();
+        const formData = new FormData();
         formData.append('file', file);
-        //uso axios per la richiesta
 
-        axios.post('/upload', { formData})
-            .then(res => {
-                console.log("res frontend =>"+res);
-                // console.log(res.data);
-               /* const { fileName, filePath } = res.data;
-                setUploadedFile({fileName, filePath});*/
-                }).catch(err => console.log("STAMPA DI ERR\n"+err))
-            //prendo dalla risposta quello che mi serve
-
-        /*}catch (err) {
-            console.log(err)
-            /!*if(err.response.status && err.response.status === 500){
-                console.log("Errore del server")
-            }else{
-                //intercetto il messaggio che arriva da backend
+        try {
+            const res = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            const { fileName, filePath } = res.data;
+            setUploadedFile({ fileName, filePath });
+        } catch (err) {
+            if (err.response.status === 500) {
+               console.log('There was a problem with the server');
+            } else {
                 console.log(err.response.data.msg);
-            }*!/
-        }*/
+            }
+        }
     };
 
     return (
         <Fragment>
-            <form onSubmit={onSubmit}>
+            <form encType="multipart/form-data" onSubmit={onSubmit}>
                 <div className="custom-file mb-4">
                     <input name="file" type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
                     <label className="custom-file-label" htmlFor="customFile">{filename}</label>
                 </div>
-                <input type="submit" value="caricamento" className="btn btn-primary btn-block mt-4"/>
+                <input type="submit" value="caricamento" className="btn btn-primary btn-block mt-4" accept="image/png"/>
             </form>
             { uploadedFile && (
                 <div className="row mt-5">
