3da4138468d79e434bad1fda38ff77f1a240f4d6
Salvatore Lo Coco <salvatore.lococo.slc@gmail.com>
2020-06-20 17:56:37 +0200

    Modifiche a Paginazione.js e Servizio.js

--- softbed-frontend/src/Components/Schermata Risultati/Servizio.js
+++ softbed-frontend/src/Components/Schermata Risultati/Servizio.js
@@ -3,7 +3,7 @@ import React from "react";
 function Servizio(props) {
     return (
         <div className="col-2 col-sm-6 col-lg-4 p-1 text-center text-sm-left">
-            <i className={"mr-2 text-primary fas fa-" + props.icona}/><span className="d-none d-sm-inline">{props.servizio}</span>
+            <span className={"mr-sm-2 w-20px text-center text-primary fas fa-" + props.icona}/><span className="d-none d-sm-inline">{props.servizio}</span>
         </div>
     )
 }
