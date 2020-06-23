7cf9eaa4a2a47dbfa8c5d8854a605625bd695748
mary_ <mary_pal@live.it>
2020-06-23 14:47:41 +0200

    gestione conferma della prenotazione non definitiva

--- softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteConfermate.js
+++ softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteConfermate.js
@@ -1,17 +1,8 @@
 import React, {useState} from "react";
 import Prenotazione from "./Prenotazione";
-let finestra=false;
-function visualizzaPrenotazioneConfermata(e){
-    if(finestra==false){
-        document.getElementById("prenotazioneConfermata").classList.add("collapse");
-        finestra=true;
-    }
-    else{
-        document.getElementById("prenotazioneConfermata").classList.remove("collapse");
-        finestra=false;
-    }
-}
 function RichiesteConfermate(props){
+    const [mostraContenuto, setMostraContenuto] = useState(false);
+    const toggleContenuto = () => setMostraContenuto(!mostraContenuto);
     return(
         <div>
             <div className="row d-flex justify-content-center">
@@ -25,11 +16,15 @@ function RichiesteConfermate(props){
                     <strong>confermata il: </strong>{props.dataConferma}
                 </div>
                 <div  className=" col-12 col-lg-3 d-flex justify-content-around">
-                    <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}}>Dichiarazione ospiti</button>
-                    <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}} onClick={visualizzaPrenotazioneConfermata}>Visualizza</button>
+                    {/* <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}}>Dichiarazione ospiti</button>
+                    <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}} onClick={toggleContenuto}>{mostraContenuto ? "Mostra meno" : "Visualizza"}</button> */}
+                    <div className=" col-12 col-lg-3 d-flex justify-content-around">
+                        <button id="dichiaraOspiti" type="button" className="btn btn-primary mt-2 mr-2"style={{width: 170 + 'px'}}>Dichiarazione ospiti</button>
+                        <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}} onClick={toggleContenuto}>{mostraContenuto ? "Mostra meno" : "Visualizza"}</button>
+                    </div>
                 </div>
             </div>
-            <div id="prenotazioneConfermata" className="collapse">
+            <div id="prenotazioneConfermata" className={"col-12 my-3 py-3 border-top border-dark" + ((mostraContenuto) ? "" : " collapse")}>
                    <Prenotazione  dataCheckIn={props.checkIn} dataCheckOut={props.checkOut} dataConferma={props.dataConferma} idPrenotazione={props.idPrenotazione} nAdulti={props.nAdulti} nBambini={props.nBambini} nEsenti={props.nEsenti} costo={props.costo} metodoPagamento={props.metodoPagamento} utente={props.refUtente}></Prenotazione>
             </div>
         </div>
--- softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteInAttesa.js
+++ softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteInAttesa.js
@@ -3,37 +3,28 @@ import React, {useState} from "react";
 import SchermataPrenotazioneStruttura from "../SchermataPrenotazioneStruttura";
 import $ from 'jquery';
 function RichiesteInAttesa(props){
-    const [prenotazioneNonConfermata, setRichiestePendenti]=useState(props.listaPrenotazioniNonConfermate);
-    const [prenotazioneConfermata, aggiornaPrenotazioniConfermate]=useState(props.listaPrenotazioniConfermate);
+    const [prenotazioniNonConfermate, setRichiestePendenti]=useState(props.listaPrenotazioniNonConfermate);
+    const [prenotazioniConfermate, aggiornaPrenotazioniConfermate]=useState(props.listaPrenotazioniConfermate);
+    const [mostraContenuto, setMostraContenuto] = useState(false);
+    const toggleContenuto = () => setMostraContenuto(!mostraContenuto);
 
-    let finestra=false;
-    function visualizzaPrenotazioneInAttesa(e){
-        if(finestra==false){
-            document.getElementById("prenotazioneInAttesa").classList.add("collapse");
-            finestra=true;
-        }
-        else{
-            document.getElementById("prenotazioneInAttesa").classList.remove("collapse");
-            finestra=false;
-        }
-    }
-    function confermaPrenotazione(e){
+    function confermaPrenotazione(){
         {/*TODO inviare richiesta al beckend*/}
-        /* document.getElementById("buttonRemove").remove();
-       let elemento=document.getElementById("listPrenotazioneConfermate").appendChild(document.getElementById("richiesta"));
-        document.getElementById("dichiaraOspiti").classList.remove("disabled");
-        elemento.classList.add("list-group-item");
-        elemento.classList.add("list-group-item-success"); */
-        console.log(prenotazioneConfermata);
-        console.log(prenotazioneNonConfermata);
-        props.aggiornaPrenotazioniConfermate(prenotazioneConfermata.push({nome: props.nome, checkIn: props.checkIn, checkOut:props.checkOut, costo:props.costo, nBambini: props.nBambini }));
+        let tmp = [...prenotazioniConfermate];
+        tmp.push(prenotazioniNonConfermate[props.indiceElemento]);
+        props.aggiornaPrenotazioniConfermate(tmp);
+        console.log(prenotazioniConfermate);
+        /* eliminazione della prenotazione dalle prenotazioni confermate*/
+        let arg=[...prenotazioniNonConfermate];
+        delete arg[props.indiceElemento];
+        props.setRichiestePendenti(arg);
     }
     function rifiutaPrenotazione(e){
         {/*TODO inviare richiesta al beckend*/}
-        document.getElementById("richiesta").parentElement.remove();
-
+        let arg=[...prenotazioniNonConfermate];
+        delete arg[props.indiceElemento];
+        props.setRichiestePendenti(arg);
     }
-
     return(
         <div id="richiesta" >
             <div className="row  d-flex justify-content-center">
@@ -48,13 +39,13 @@ function RichiesteInAttesa(props){
                 </div>
                 <div className=" col-12 col-lg-3 d-flex justify-content-around">
                     <button id="dichiaraOspiti" type="button" className="btn btn-primary mt-2 mr-2 disabled"style={{width: 170 + 'px'}}>Dichiarazione ospiti</button>
-                    <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}} onClick={visualizzaPrenotazioneInAttesa}>Visualizza</button>
+                    <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}} onClick={toggleContenuto}>{mostraContenuto ? "Mostra meno" : "Visualizza"}</button>
                 </div>
             </div>
-            <div id="prenotazioneInAttesa" className="collapse">
+            <div id="prenotazioneInAttesa" className={"col-12 my-3 py-3 border-top border-dark" + ((mostraContenuto) ? "" : " collapse")}>
                 <Prenotazione dataCheckIn={props.checkIn} dataCheckOut={props.checkOut} dataConferma={props.dataConferma} idPrenotazione={props.idPrenotazione} nAdulti={props.nAdulti} nBambini={props.nBambini} nEsenti={props.nEsenti} costo={props.costo} metodoPagamento={props.metodoPagamento} utente={props.refUtente}></Prenotazione>
                 <div id="buttonRemove" className="btn-group d-flex justify-content-between">
-                    <button type="button " className="btn btn-success mr-2" style={{maxWidth : 170+'px'}} onClick={confermaPrenotazione(props)}>Conferma</button>
+                    <button type="button " className="btn btn-success mr-2" style={{maxWidth : 170+'px'}} onClick={confermaPrenotazione}>Conferma</button>
                     <button type="button " className="btn btn-danger mr-2"style={{maxWidth : 170+'px'}} onClick={rifiutaPrenotazione}>Rifiuta</button>
                 </div>
             </div>
