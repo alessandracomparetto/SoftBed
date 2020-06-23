fb6420ac3c2aa8ee8b5e663eeb28504761d63156
Salvatore Lo Coco <salvatore.lococo.slc@gmail.com>
2020-06-23 15:38:50 +0200

    Aggiornate SchermataPrenotazioneStruttura.js e RichiesteInAttesa.js

--- softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteInAttesa.js
+++ softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteInAttesa.js
@@ -3,28 +3,9 @@ import React, {useState} from "react";
 import SchermataPrenotazioneStruttura from "../SchermataPrenotazioneStruttura";
 import $ from 'jquery';
 function RichiesteInAttesa(props){
-    const [prenotazioniNonConfermate, setRichiestePendenti]=useState(props.listaPrenotazioniNonConfermate);
-    const [prenotazioniConfermate, aggiornaPrenotazioniConfermate]=useState(props.listaPrenotazioniConfermate);
     const [mostraContenuto, setMostraContenuto] = useState(false);
     const toggleContenuto = () => setMostraContenuto(!mostraContenuto);
 
-    function confermaPrenotazione(){
-        {/*TODO inviare richiesta al beckend*/}
-        let tmp = [...prenotazioniConfermate];
-        tmp.push(prenotazioniNonConfermate[props.indiceElemento]);
-        props.aggiornaPrenotazioniConfermate(tmp);
-        console.log(prenotazioniConfermate);
-        /* eliminazione della prenotazione dalle prenotazioni confermate*/
-        let arg=[...prenotazioniNonConfermate];
-        delete arg[props.indiceElemento];
-        props.setRichiestePendenti(arg);
-    }
-    function rifiutaPrenotazione(e){
-        {/*TODO inviare richiesta al beckend*/}
-        let arg=[...prenotazioniNonConfermate];
-        delete arg[props.indiceElemento];
-        props.setRichiestePendenti(arg);
-    }
     return(
         <div id="richiesta" >
             <div className="row  d-flex justify-content-center">
@@ -45,8 +26,8 @@ function RichiesteInAttesa(props){
             <div id="prenotazioneInAttesa" className={"col-12 my-3 py-3 border-top border-dark" + ((mostraContenuto) ? "" : " collapse")}>
                 <Prenotazione dataCheckIn={props.checkIn} dataCheckOut={props.checkOut} dataConferma={props.dataConferma} idPrenotazione={props.idPrenotazione} nAdulti={props.nAdulti} nBambini={props.nBambini} nEsenti={props.nEsenti} costo={props.costo} metodoPagamento={props.metodoPagamento} utente={props.refUtente}></Prenotazione>
                 <div id="buttonRemove" className="btn-group d-flex justify-content-between">
-                    <button type="button " className="btn btn-success mr-2" style={{maxWidth : 170+'px'}} onClick={confermaPrenotazione}>Conferma</button>
-                    <button type="button " className="btn btn-danger mr-2"style={{maxWidth : 170+'px'}} onClick={rifiutaPrenotazione}>Rifiuta</button>
+                    <button type="button " className="btn btn-success mr-2" style={{maxWidth : 170+'px'}} onClick={() => props.confermaPrenotazione(props.indiceElemento)}>Conferma</button>
+                    <button type="button " className="btn btn-danger mr-2" style={{maxWidth : 170+'px'}} onClick={() => props.rifiutaPrenotazione(props.indiceElemento)}>Rifiuta</button>
                 </div>
             </div>
         </div>
--- softbed-frontend/src/Components/SchermataPrenotazioneStruttura.js
+++ softbed-frontend/src/Components/SchermataPrenotazioneStruttura.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import RichiesteInAttesa from "./Schermata prenotazione struttura/RichiesteInAttesa";
 import RichiesteConfermate from "./Schermata prenotazione struttura/RichiesteConfermate";
 function SchermataPrenotazioneStruttura(){
-    const [listaPrenotazioniNonConfermate, setRichiestePendenti]=useState([{idPrenotazione: "3333",
+    const [listaPrenotazioniNonConfermate, setRichiestePendenti] = useState([{idPrenotazione: "3333",
         nome: "Struttura 1",
         checkIn: "22/06/2020",
         checkOut: "22/06/2020",
@@ -46,35 +46,61 @@ function SchermataPrenotazioneStruttura(){
         dataScadenza: "5/06/2020",
         metodoPagamento:"loco",
         refUtente:"1111"}]);
-    const [listaPrenotazioniConfermate, aggiornaPrenotazioniConfermate]=useState([{idPrenotazione: "3333",
-        nome: "Struttura 1",
-        checkIn: "22/06/2020",
-        checkOut: "22/06/2020",
-        costo:"10",
-        nBambini:"3",
-        nAdulti:"3",
-        nEsenti:"1",
-        confermata:"true",
-        dichiarazioneOspiti:"false",
-        struttura:"1",
-        dataConferma:"22/06/2020",
-        dataScadenza:"22/06/2020",
-        metodoPagamento:"online",
-        refUtente:"1111"
-    }], { nome: "Struttura 100",
-        checkIn: "22/06/2023",
-        checkOut: "22/06/2023",
-        costo:"10",
-        nBambini:"3",
-        nAdulti:"3",
-        nEsenti:"1",
-        confermata:"true",
-        dichiarazioneOspiti:"false",
-        struttura:"1",
-        dataConferma:"22/06/1020",
-        dataScadenza:"22/06/1020",
-        metodoPagamento:"online",
-        refUtente:"1111"});
+    const [listaPrenotazioniConfermate, aggiornaPrenotazioniConfermate] = useState([
+        {
+            idPrenotazione: "3333",
+            nome: "Struttura 1",
+            checkIn: "22/06/2020",
+            checkOut: "22/06/2020",
+            costo:"10",
+            nBambini:"3",
+            nAdulti:"3",
+            nEsenti:"1",
+            confermata:"true",
+            dichiarazioneOspiti:"false",
+            struttura:"1",
+            dataConferma:"22/06/2020",
+            dataScadenza:"22/06/2020",
+            metodoPagamento:"online",
+            refUtente:"1111"
+        },
+        {
+            nome: "Struttura 100",
+            checkIn: "22/06/2023",
+            checkOut: "22/06/2023",
+            costo:"10",
+            nBambini:"3",
+            nAdulti:"3",
+            nEsenti:"1",
+            confermata:"true",
+            dichiarazioneOspiti:"false",
+            struttura:"1",
+            dataConferma:"22/06/1020",
+            dataScadenza:"22/06/1020",
+            metodoPagamento:"online",
+            refUtente:"1111"
+        }]);
+
+    const rifiutaPrenotazione = (indice) => {
+        // Rimuovere dalla lista in attesa
+        let tmp = [...listaPrenotazioniNonConfermate];
+        tmp.splice(indice, 1);
+        setRichiestePendenti(tmp);
+    }
+
+    const confermaPrenotazione = (indice) => {
+        // Aggiungere alla lista confermate
+        let tmp = [...listaPrenotazioniConfermate];
+        tmp.push(listaPrenotazioniNonConfermate[indice]);
+        aggiornaPrenotazioniConfermate(tmp);
+
+        // Rimuovere dalla lista in attesa
+        tmp = [...listaPrenotazioniNonConfermate];
+        tmp.splice(indice, 1);
+        setRichiestePendenti(tmp);
+    }
+
+
     return(
         <div className="container">
             <h4>Richieste in attesa</h4>
@@ -83,7 +109,7 @@ function SchermataPrenotazioneStruttura(){
                     listaPrenotazioniNonConfermate.map((prenotazioni, indice) => {
                         console.log("Lista:"+listaPrenotazioniNonConfermate);
 
-                            return <li className="list-group-item list-group-item-warning"><RichiesteInAttesa key={indice} indiceElemento={indice} dataCheckIn={prenotazioni.checkIn} dataCheckOut={prenotazioni.checkOut} dataScadenza={prenotazioni.dataScadenza} idPrenotazione={prenotazioni.idPrenotazione} nAdulti={prenotazioni.nAdulti} nBambini={prenotazioni.nBambini} costo={prenotazioni.costo} nEsenti={prenotazioni.nEsenti} metodoPagamento={prenotazioni.metodoPagamento} aggiornaPrenotazioniConfermate={aggiornaPrenotazioniConfermate} listaPrenotazioniConfermate={listaPrenotazioniConfermate}  setRichiestePendenti={setRichiestePendenti} listaPrenotazioniNonConfermate={listaPrenotazioniNonConfermate}/></li>
+                            return <li className="list-group-item list-group-item-warning"><RichiesteInAttesa key={indice} indiceElemento={indice} dataCheckIn={prenotazioni.checkIn} dataCheckOut={prenotazioni.checkOut} dataScadenza={prenotazioni.dataScadenza} idPrenotazione={prenotazioni.idPrenotazione} nAdulti={prenotazioni.nAdulti} nBambini={prenotazioni.nBambini} costo={prenotazioni.costo} nEsenti={prenotazioni.nEsenti} metodoPagamento={prenotazioni.metodoPagamento} rifiutaPrenotazione={rifiutaPrenotazione} confermaPrenotazione={confermaPrenotazione}/></li>
                         })
 
                 }
