c77d3595711d3d59ca36686a99137611e6b74523
Mariagrazia98 <55098208+Mariagrazia98@users.noreply.github.com>
2020-06-23 16:48:57 +0200

    Update RichiesteInAttesa.js

--- softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteInAttesa.js
+++ softbed-frontend/src/Components/Schermata prenotazione struttura/RichiesteInAttesa.js
@@ -1,13 +1,11 @@
-import Prenotazione from "./Prenotazione";
+import PrenotazioneGestore from "./PrenotazioneGestore";
 import React, {useState} from "react";
-import SchermataPrenotazioneStruttura from "../SchermataPrenotazioneStruttura";
-import $ from 'jquery';
+
 function RichiesteInAttesa(props){
     const [mostraContenuto, setMostraContenuto] = useState(false);
     const toggleContenuto = () => setMostraContenuto(!mostraContenuto);
-
     return(
-        <div id="richiesta" >
+        <div >
             <div className="row  d-flex justify-content-center">
                 <div className="col-4 col-md-3  mt-lg-4">
                     <strong>check-in: </strong>{props.dataCheckIn}
@@ -19,14 +17,14 @@ function RichiesteInAttesa(props){
                     <strong>scadenza: </strong>{props.dataScadenza}
                 </div>
                 <div className=" col-12 col-lg-3 d-flex justify-content-around">
-                    <button id="dichiaraOspiti" type="button" className="btn btn-primary mt-2 mr-2 disabled"style={{width: 170 + 'px'}}>Dichiarazione ospiti</button>
-                    <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px'}} onClick={toggleContenuto}>{mostraContenuto ? "Mostra meno" : "Visualizza"}</button>
+                    <button id="dichiaraOspiti" type="button" className="btn btn-primary mt-2 mr-2 disabled" style={{width: 170 + 'px',minWidth:130+'px'}}>Dichiarazione ospiti</button>
+                    <button type="button" className="btn btn-primary mt-2 mr-2 " style={{width: 170 + 'px', minWidth:130+'px'}} onClick={toggleContenuto}>{mostraContenuto ? "Mostra meno" : "Visualizza"}</button>
                 </div>
             </div>
-            <div id="prenotazioneInAttesa" className={"col-12 my-3 py-3 border-top border-dark" + ((mostraContenuto) ? "" : " collapse")}>
-                <Prenotazione dataCheckIn={props.checkIn} dataCheckOut={props.checkOut} dataConferma={props.dataConferma} idPrenotazione={props.idPrenotazione} nAdulti={props.nAdulti} nBambini={props.nBambini} nEsenti={props.nEsenti} costo={props.costo} metodoPagamento={props.metodoPagamento} utente={props.refUtente}></Prenotazione>
+            <div className={"col-12 my-3 py-3 border-top border-dark" + ((mostraContenuto) ? "" : " collapse")}>
+                <PrenotazioneGestore dataCheckIn={props.checkIn} dataCheckOut={props.checkOut} dataConferma={props.dataConferma} idPrenotazione={props.idPrenotazione} nAdulti={props.nAdulti} nBambini={props.nBambini} nEsenti={props.nEsenti} costo={props.costo} metodoPagamento={props.metodoPagamento} utente={props.refUtente}></PrenotazioneGestore>
                 <div id="buttonRemove" className="btn-group d-flex justify-content-between">
-                    <button type="button " className="btn btn-success mr-2" style={{maxWidth : 170+'px'}} onClick={() => props.confermaPrenotazione(props.indiceElemento)}>Conferma</button>
+                    <button type="button " className="btn  btn-success mr-2" style={{maxWidth : 170+'px'}} onClick={() => props.confermaPrenotazione(props.indiceElemento)}>Conferma</button>
                     <button type="button " className="btn btn-danger mr-2" style={{maxWidth : 170+'px'}} onClick={() => props.rifiutaPrenotazione(props.indiceElemento)}>Rifiuta</button>
                 </div>
             </div>
